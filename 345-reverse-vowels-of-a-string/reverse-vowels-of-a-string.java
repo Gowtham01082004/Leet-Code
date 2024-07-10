f1305a30c0e9b5df099a077aa1d3a5c081ebbe34
Gowtham01082004 <144901847+Gowtham01082004@users.noreply.github.com>
2024-07-10 22:22:26 +0530

    Time: 2 ms (98.73%) | Memory: 44.7 MB (70.93%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,32 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
-        char[] ch = s.toCharArray();
+        char [] ch=s.toCharArray();
         int i = 0, j = ch.length - 1;
 
-        for (i = 0, j = ch.length - 1; i < j; ) {
- 
-            if (!isVowel(ch[i])) {
+        for(i=0 ,j=ch.length-1;i<j;){
+            if(!isvowel(ch[i])){
                 i++;
                 continue;
             }
-
-            if (!isVowel(ch[j])) {
+             if(!isvowel(ch[j])){
                 j--;
                 continue;
             }
-
-            char temp = ch[i];
-            ch[i] = ch[j];
-            ch[j] = temp;
-           
+            char temp=ch[i];
+            ch[i]=ch[j];
+            ch[j]=temp;
             i++;
             j--;
         }
-
-        return new String(ch);
+        String ans=new String(ch);
+        return ans;
     }
-
-    private boolean isVowel(char c) {
+    private boolean isvowel(char c){
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
