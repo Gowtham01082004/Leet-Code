e785d8d80d1d40ea67cb9d0adf7ed05136f27010
Gowtham01082004 <144901847+Gowtham01082004@users.noreply.github.com>
2024-07-30 17:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (91.55%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,20 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int bitcount[]=new int[32];
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<32;j++){
-                if((nums[i]&(1<< j))!=0){
-                    bitcount[j]++;
-                }
-            }
+       int ones = 0, twos = 0;
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
         }
-        int result=0;
-        for(int i=0;i<32;i++){
-            if(bitcount[i]%3!=0){
-                result=result|(1<<i);
-            }
-        }
-        return result;
-
+        return ones;
     }
 }
