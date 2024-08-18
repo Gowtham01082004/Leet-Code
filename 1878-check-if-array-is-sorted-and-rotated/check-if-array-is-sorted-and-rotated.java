bd903cfd9eb24a035929a738209a5520f5eb90cb
Gowtham01082004 <144901847+Gowtham01082004@users.noreply.github.com>
2024-08-18 11:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (16.04%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean check(int[] nums) {
-        int count = 0;
-        int n = nums.length;
-        
-        for (int i = 0; i < n; i++) {
-            
-            if (nums[i] > nums[(i + 1) % n]) {
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]){
                 count++;
             }
-            // More than 1 break point means array can't be rotated to be non-decreasing
-            if (count > 1) {
+            if(count>1){
                 return false;
             }
         }
