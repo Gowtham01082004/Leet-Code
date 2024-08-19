3c8118f5c6f182a4be474765c694acb07110abf1
Gowtham01082004 <144901847+Gowtham01082004@users.noreply.github.com>
2024-08-19 23:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (48.63%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int a = 0;
+        int ans = 0;
         while(n != 1){
             if(n%2==0){
-                a+=(n/2);
+                ans+=(n/2);
                 n/=2;
             }
             else{
-                a+=((n-1)/2);
+                ans+=((n-1)/2);
                 n=((n-1)/2)+1;
             }
         }
-        return a;
+        return ans;
     }
 }
