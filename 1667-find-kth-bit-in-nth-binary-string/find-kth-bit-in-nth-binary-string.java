f0129e34e5dd36b6825a958ddf7538f7e84be513
Gowtham01082004 <144901847+Gowtham01082004@users.noreply.github.com>
2024-10-19 17:03:18 +0530

    Time: 105 ms (29.96%) | Memory: 55.1 MB (9.34%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,30 +1,22 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        // Base case: when n is 1, the string is "0"
         if (n == 1) {
             return '0';
         }
         
         String first = "0";
         String second = "";
-        
-        // Generate the Sn sequence iteratively
-        for (int i = 2; i <= n; i++) {  // Start from 2 since S1 is "0"
-            second = first + "1" + reverse(invert(first));  // Build the sequence
-            first = second;  // Update first for next iteration
+        for (int i = 2; i <= n; i++) {  
+            second = first + "1" + reverse(invert(first));  
+            first = second;  
         }
-        
-        // Return the kth bit from the generated string
         return second.charAt(k - 1);  
     }
 
-    // Function to reverse a string
     public String reverse(String s) {
         StringBuilder sb = new StringBuilder(s);  
         return sb.reverse().toString();
     }
-
-    // Function to invert the string (change '0' -> '1' and '1' -> '0')
     public String invert(String s) {
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
