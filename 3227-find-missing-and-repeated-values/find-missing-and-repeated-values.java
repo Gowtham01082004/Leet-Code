b1686770dedc1e6d8f71e1652fc77e80719f5936
Gowtham01082004 <144901847+Gowtham01082004@users.noreply.github.com>
2025-04-27 20:52:20 +0530

    Time: 6 ms (24.33%) | Memory: 45.7 MB (46.65%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,40 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                int n=grid[i][j];
-                map.put(n,map.getOrDefault(n,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Count frequencies
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                int num = grid[i][j];
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
-        }
-        int m=0;
-        ArrayList<Integer> list=new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            list.add(entry.getKey());
-            if(entry.getValue()>1){
-                 m=entry.getKey();
-            }
-        }
-        int count =1;
-        Collections.sort(list);
-        int o=0;
-        for(int i=0;i<list.size();i++){
-          if(list.get(i)==count){
-            count++;
-          }else{
-            break;
-          }
-        }
-        if(count==list.size()){
-            o=count+1;
-        }else{
-            o=count;
         }
 
-        
-        int ans[]=new int[2];
-        ans[0]=m;
-        ans[1]=count;
-        return ans;
+        int n = grid.length;
+        int repeated = 0, missing = 0;
+
+        // Check from 1 to n*n
+        for (int i = 1; i <= n * n; i++) {
+            int freq = map.getOrDefault(i, 0);
+            if (freq == 2) {
+                repeated = i;
+            } else if (freq == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{repeated, missing};
     }
 }
